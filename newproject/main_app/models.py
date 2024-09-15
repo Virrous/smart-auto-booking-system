b9cb1c53c727eb1cc8a82b0fc750d84bd01c607f
Manjil Timsina <123795262+its-manzil@users.noreply.github.com>
2024-09-15 23:37:18 +0545

    super updated

--- newproject/main_app/models.py
+++ newproject/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, choices=[
+        ('Jhapa', 'Jhapa'),
+        ('Ilam', 'Ilam'),
+        ('Biratnagar', 'Biratnagar'),
+    ])
+
+    def __str__(self):
+        return self.email
